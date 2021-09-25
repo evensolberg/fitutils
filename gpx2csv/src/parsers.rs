8fdd09e95a4642a0b039f67dd502f61c09f96a97
Even Solberg <even.solberg@gmail.com>
2021-09-25 16:58:30 -0700

    g2c: More types, more robust parsing

--- gpx2csv/src/parsers.rs
+++ gpx2csv/src/parsers.rs
@@ -19,17 +19,75 @@ fn gpx_ver_to_string(version: &gpx::GpxVersion) -> String {
 pub fn parse_gpx_header(gpx_data: &Gpx) -> Result<types::GpxMetadata, Box<dyn Error>> {
     let mut gpx_meta = types::GpxMetadata::new();
 
-    // FIXME: There has to be a cleaner way to do this!
+    // Parse the GPX header
+    log::trace!("parsers::parse_gpx_header() -- Parsing the GPX header information.");
     gpx_meta.version = Some(gpx_ver_to_string(&gpx_data.version));
-    gpx_meta.creator = Some(gpx_data.creator.as_ref().unwrap().to_string());
+    if let Some(creator) = &gpx_data.creator {
+        gpx_meta.creator = Some(creator.to_string());
+    }
 
+    // Parse the metadata
     let metadata = gpx_data.metadata.as_ref().unwrap();
-    gpx_meta.activity = Some(metadata.name.as_ref().unwrap().to_string());
-    gpx_meta.description = Some(metadata.description.as_ref().unwrap().to_string());
-    gpx_meta.author = Some(metadata.name.as_ref().unwrap().to_string());
-    gpx_meta.links_href = Some(metadata.links[0].href.to_string());
-    gpx_meta.time = Some(metadata.time.unwrap());
+    if let Some(activity) = &metadata.name {
+        gpx_meta.activity = Some(activity.to_string());
+    }
+    if let Some(description) = &metadata.description {
+        gpx_meta.description = Some(description.to_string());
+    }
+    if let Some(keywords) = &metadata.keywords {
+        gpx_meta.keywords = Some(keywords.to_string());
+    }
 
-    // return it
+    if let Some(time) = &metadata.time {
+        gpx_meta.time = Some(time.to_owned());
+    }
+    // For now, only read the first href in the list of links (if there is one)
+    if metadata.links.len() > 0 {
+        gpx_meta.links_href = Some(metadata.links[0].href.to_string());
+        if let Some(text) = &metadata.links[0].text {
+            gpx_meta.links_text = Some(text.to_string());
+        }
+    }
+
+    // See if we have copyright information, and extract it if we do.
+    match &metadata.copyright {
+        Some(cr_data) => {
+            log::trace!("parsers::parse_gpx_header() -- Copyright information found. Parsing.");
+            if let Some(author) = &cr_data.author {
+                gpx_meta.copyright_author = Some(author.to_string())
+            };
+            if let Some(license) = &cr_data.license {
+                gpx_meta.copyright_license = Some(license.to_string())
+            };
+            if let Some(year) = cr_data.year {
+                gpx_meta.copyright_year = Some(year)
+            };
+        }
+        None => {
+            log::trace!(
+                "parsers::parse_gpx_header() -- Copyright information not found. No need to parse."
+            );
+        }
+    }
+
+    // Parse metadata.author if there is anything there.
+    match &metadata.author {
+        Some(author) => {
+            log::trace!("parsers::parse_gpx_header() -- Author information found. Parsing.");
+            if let Some(name) = &author.name {
+                gpx_meta.author_name = Some(name.to_string())
+            };
+            if let Some(email) = &author.email {
+                gpx_meta.author_email = Some(email.to_string())
+            };
+        }
+        None => {
+            log::trace!(
+                "parsers::parse_gpx_header() -- Author information not found. No need to parse."
+            );
+        }
+    }
+
+    // return the metadata struct
     Ok(gpx_meta)
 }
